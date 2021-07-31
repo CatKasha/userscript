202e4fcf7e37abe73022f75aa239c176b9a4a509
CatKasha <CatKasha@users.noreply.github.com>
2021-07-31 11:24:14 +0300

    fixed redirect with .jpg and .png at the end of url

--- twitter_orig_image.user.js
+++ twitter_orig_image.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Twitter orig image
 // @description    Redirects image from Twitter to its highest quality
-// @version        0.1.0
+// @version        0.1.1
 // @match          https://pbs.twimg.com/media/*
 // @run-at         document-start
 // ==/UserScript==
@@ -19,6 +19,6 @@ if (current_url.match(/^https:\/\/pbs\.twimg\.com\/media\/[A-Za-z0-9_-]{15}.(?:j
 }
 
 if (current_url.match(/^https:\/\/pbs\.twimg\.com\/media\/[A-Za-z0-9_-]{15}.(?:jpg|png)$/) && current_url.split(":")[(current_url.split(":").length - 1)] !== "orig") {
-    let new_url = current_url.match(/^https:\/\/pbs\.twimg\.com\/media\/[A-Za-z0-9_-]{15}.(?:jpg|png):/)[0] + "orig";
+    let new_url = current_url.match(/^https:\/\/pbs\.twimg\.com\/media\/[A-Za-z0-9_-]{15}.(?:jpg|png)/)[0] + ":orig";
     window.location.assign(new_url);
 }
