0499a683c9840084157010988607f867ab027cf5
CatKasha <CatKasha@users.noreply.github.com>
2023-04-11 02:12:29 +0300

    a little bit of refactoring

--- twitter_orig_image.user.js
+++ twitter_orig_image.user.js
@@ -1,18 +1,21 @@
 // ==UserScript==
 // @name           Twitter orig image
 // @description    Redirects image from Twitter to its highest quality
-// @version        0.1.2
+// @version        0.2.0
 // @match          https://pbs.twimg.com/media/*
 // @run-at         document-start
 // ==/UserScript==
 
 let current_url = window.location.href;
+let image_url_regex = /^https:\/\/pbs\.twimg\.com\/media\/[A-Za-z0-9_-]{15}(?:\?format=(?:jpg|png)|\.(?:jpg|png))/;
+let new_url = "";
 
-if (current_url.match(/https:\/\/pbs\.twimg\.com\/media\/[A-Za-z0-9_-]{15}\?format=(?:jpg|png)&name=/) && current_url.split("=")[(current_url.split("=").length - 1)] !== "orig") {
-    let new_url = current_url.match(/https:\/\/pbs\.twimg\.com\/media\/[A-Za-z0-9_-]{15}\?format=(?:jpg|png)&name=/)[0] + "orig";
-    window.location.assign(new_url);
-}
-else if (current_url.match(/https:\/\/pbs\.twimg\.com\/media\/[A-Za-z0-9_-]{15}\.(?:jpg|png)/) && current_url.split(":")[(current_url.split(":").length - 1)] !== "orig") {
-    let new_url = current_url.match(/https:\/\/pbs\.twimg\.com\/media\/[A-Za-z0-9_-]{15}\.(?:jpg|png)/)[0] + ":orig";
+if (current_url.slice(-4) !== "orig" && image_url_regex.test(current_url) === true) {
+    if (current_url[43] === "?") {
+        new_url = current_url.match(image_url_regex)[0] + "&name=orig";
+    } else {
+        new_url = current_url.match(image_url_regex)[0] + ":orig";
+    }
+
     window.location.assign(new_url);
 }
