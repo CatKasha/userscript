90e1a4d7b5ee0dba352b649667d1e6b02d878e4a
CatKasha <CatKasha@users.noreply.github.com>
2023-05-11 00:48:25 +0300

    added avatar support

--- discord_orig_image.user.js
+++ discord_orig_image.user.js
@@ -1,14 +1,22 @@
 // ==UserScript==
 // @name           Discord orig image
 // @description    Get rid of width and height from url
-// @version        0.1.0
+// @version        0.2.0
 // @match          https://media.discordapp.net/attachments/*
+// @match          https://cdn.discordapp.com/avatars/*
+// @match          https://cdn.discordapp.com/guilds/*/users/*/avatars/*
 // @run-at         document-start
 // ==/UserScript==
 
 let current_url = window.location.href;
 
 if (/^https:\/\/media\.discordapp\.net\/attachments\/\d*\/\d*\/\S*\.(?:jpg|png)\?width=\d*&height=\d*/.test(current_url) === true) {
-    let new_url = current_url.match(/^https:\/\/media\.discordapp\.net\/attachments\/\d*\/\d*\/\S*\.(?:jpg|png)/)[0];
-    window.location.assign(new_url);
+    window.location.assign(current_url.match(/^https:\/\/media\.discordapp\.net\/attachments\/\d*\/\d*\/\S*\.(?:jpg|png)/)[0]);
+}
+
+if (/^https:\/\/cdn\.discordapp\.com\/(?:avatars\/\d*|guilds\/\d*\/users\/\d*\/avatars)\/\S*\.(?:webp|gif)/.test(current_url) === true) {
+    let new_url = current_url.match(/^https:\/\/cdn\.discordapp\.com\/(?:avatars\/\d*|guilds\/\d*\/users\/\d*\/avatars)\/\S*\.(?:webp|gif)/)[0];
+    if (current_url.slice(new_url.length) !== "?size=2048") {
+        window.location.assign(new_url + "?size=2048");
+    }
 }
