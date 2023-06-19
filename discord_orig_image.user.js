e11b77fd9e41ab73775911633665029f39c17185
CatKasha <CatKasha@users.noreply.github.com>
2023-06-19 18:08:08 +0300

    added attachment gif detection

--- discord_orig_image.user.js
+++ discord_orig_image.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Discord orig image
 // @description    Get rid of width and height from url
-// @version        0.2.0
+// @version        0.2.1
 // @match          https://media.discordapp.net/attachments/*
 // @match          https://cdn.discordapp.com/avatars/*
 // @match          https://cdn.discordapp.com/guilds/*/users/*/avatars/*
@@ -10,12 +10,17 @@
 
 let current_url = window.location.href;
 
-if (/^https:\/\/media\.discordapp\.net\/attachments\/\d*\/\d*\/\S*\.(?:jpg|png)\?width=\d*&height=\d*/.test(current_url) === true) {
-    window.location.assign(current_url.match(/^https:\/\/media\.discordapp\.net\/attachments\/\d*\/\d*\/\S*\.(?:jpg|png)/)[0]);
-}
+let regex_attachment = /^https:\/\/media\.discordapp\.net\/attachments\/\d*\/\d*\/\S*\.(?:jpg|png|gif)/
+let regex_avatar = /^https:\/\/cdn\.discordapp\.com\/(?:avatars\/\d*|guilds\/\d*\/users\/\d*\/avatars)\/\S*\.(?:webp|gif)/
 
-if (/^https:\/\/cdn\.discordapp\.com\/(?:avatars\/\d*|guilds\/\d*\/users\/\d*\/avatars)\/\S*\.(?:webp|gif)/.test(current_url) === true) {
-    let new_url = current_url.match(/^https:\/\/cdn\.discordapp\.com\/(?:avatars\/\d*|guilds\/\d*\/users\/\d*\/avatars)\/\S*\.(?:webp|gif)/)[0];
+
+if (regex_attachment.test(current_url) === true) {
+    let new_url = current_url.match(regex_attachment)[0]
+    if (current_url.length > new_url.length) {
+        window.location.assign(new_url);
+    }
+} else if (regex_avatar.test(current_url) === true) {
+    let new_url = current_url.match(regex_avatar)[0];
     if (current_url.slice(new_url.length) !== "?size=2048") {
         window.location.assign(new_url + "?size=2048");
     }
