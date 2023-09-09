a3618f449608ae52527c2fef0488a5c8b3ad87f5
CatKasha <CatKasha@users.noreply.github.com>
2023-09-09 17:15:42 +0300

    combine twitter and discord userscript to one

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ Some of my userscripts, tested in Firefox with Greasemonkey
 * Bandcamp volume control - Adds volume control in Bandcamp  
 [Source](https://github.com/CatKasha/userscript/blob/main/bandcamp_volume_control.user.js) | [Install](https://github.com/CatKasha/userscript/raw/main/bandcamp_volume_control.user.js)
 
-* Twitter orig image - Redirects image from Twitter to its highest quality  
-[Source](https://github.com/CatKasha/userscript/blob/main/twitter_orig_image.user.js) | [Install](https://github.com/CatKasha/userscript/raw/main/twitter_orig_image.user.js)
-
-* Discord orig image - Get rid of width and height from url  
-[Source](https://github.com/CatKasha/userscript/blob/main/discord_orig_image.user.js) | [Install](https://github.com/CatKasha/userscript/raw/main/discord_orig_image.user.js)
+* HQ image redirect - Redirects image url to its highest quality  
+Twitter - redirects to orig  
+Discord - removes width=&height= from attachments, redirects user avatar and user custom avatar on server to size=4096  
+Twitch - redirects avatar to 600x600  
+[Source](https://github.com/CatKasha/userscript/blob/main/hq_image_redirect.user.js) | [Install](https://github.com/CatKasha/userscript/raw/main/hq_image_redirect.user.js)
 
 * Select text in Web Spotify - Removes restriction to select text in Web version of Spotify  
 Originally made for selecting song name on the left side of the player, but may works in other places (hold Alt button when selecting text to avoid moving things)  
---
+++ hq_image_redirect.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name           HQ image redirect
+// @description    Redirects image url to its highest quality
+// @version        0.1.0
+// @match          https://pbs.twimg.com/media/*
+// @match          https://media.discordapp.net/attachments/*
+// @match          https://cdn.discordapp.com/avatars/*
+// @match          https://cdn.discordapp.com/guilds/*/users/*/avatars/*
+// @match          https://static-cdn.jtvnw.net/jtv_user_pictures/*
+// @run-at         document-start
+// ==/UserScript==
+
+
+// Twitter
+let current_url = window.location.href;
+let twitter_image_regex = /^https:\/\/pbs\.twimg\.com\/media\/[A-Za-z0-9_-]{15}(?:\.|\?format=)(?:jpg|png)/;
+
+if (current_url.slice(-4) !== "orig" && twitter_image_regex.test(current_url) === true) {
+    if (current_url[43] === "?") {
+        window.location.assign(current_url.match(twitter_image_regex)[0] + "&name=orig");
+    } else {
+        window.location.assign(current_url.match(twitter_image_regex)[0] + ":orig");
+    }
+}
+
+
+// Discord
+let discord_regex_attachment = /^https:\/\/media\.discordapp\.net\/attachments\/\d*\/\d*\/\S*\.(?:jpg|png|gif)/;
+let discord_regex_avatar = /^https:\/\/cdn\.discordapp\.com\/(?:avatars\/\d*|guilds\/\d*\/users\/\d*\/avatars)\/\S*\.(?:webp|gif)/;
+
+if (discord_regex_attachment.test(current_url) === true) {
+    let new_url = current_url.match(discord_regex_attachment)[0];
+    if (current_url.length > new_url.length) {
+        window.location.assign(new_url);
+    }
+}
+if (discord_regex_avatar.test(current_url) === true) {
+    let new_url = current_url.match(discord_regex_avatar)[0];
+    if (current_url.slice(new_url.length) !== "?size=4096") {
+        window.location.assign(new_url + "?size=4096");
+    }
+}
+
+
+// Twitch
+let twitch_regex_avatar = /^https:\/\/static-cdn\.jtvnw\.net\/jtv_user_pictures\/[a-z0-9-]*-profile_image(?:-[a-z0-9]{16}|)-/;
+
+if (twitch_regex_avatar.test(current_url) === true) {
+    let new_url = current_url.match(twitch_regex_avatar)[0];
+    let image_ext = "." + current_url.split(".").slice(-1)[0];
+    if (current_url.slice(new_url.length, current_url.length - image_ext.length) !== "600x600") {
+        window.location.assign(new_url + "600x600" + image_ext);
+    }
+}
